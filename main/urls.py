4ae1c45512f835fd2205ad7c193e0885d6c33cb4
EbanSnt <negociosebansnt@gmail.com>
2024-01-31 22:15:04 -0300

    Avances
    Se avanza en la creacion de los templates. Se agregan urls y  funciones de "create" and "read"

---
+++ main/urls.py
@@ -0,0 +1,16 @@
+from main.views import *
+from django.urls import path
+
+
+urlpatterns = [
+    path('', index, name="home"),
+    path('new_customer', create_customer, name="new_customer"),
+    path('new_ticket', create_ticket, name="new_ticket"),
+    path('new_warranty_product', create_warranty_product, name="new_warranty_product"),
+    path('new_delivered_product', create_delivered_product, name="new_delivered_product"),
+    path('all_customers', show_all_customers, name="all_customers"),
+    path('all_tickets', show_all_tickets, name="all_tickets"),
+    path('all_warranty_products', show_all_warranty_products, name="all_warranty_products"),
+    path('all_delivered_products', show_all_delivered_products, name="all_delivered_products"),
+
+]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,101 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from main.models import Customers,Tickets,WarrantyProducts,DeliveredProducts
 
-# Create your views here.
 
+def index(request):
+    """Vista principal"""
+    try:
+        return render(request, "index.html")
+    except Exception:
+        return render(request, "index.html")
+
+### TODOS LOS "CREATE" ###
+def create_customer(request):
+    """Crea un nuevo cliente"""
+    if request.method == "POST":
+        name = request.POST["name"]
+        telephone_number = request.POST["telephone_number"]
+        Customers.objects.create(name=name,telephone_number=telephone_number)
+        return redirect("all_customers")
+    else:
+        return render(request, "new_customer.html")
+
+
+def create_ticket(request):
+    if request.method == "POST":
+        ticket_number = request.POST["ticket_number"]
+        customer_id = request.POST["customer_id"]
+        customer = Customers.objects.get(id=customer_id)
+        product = request.POST["product"]
+        trademark = request.POST["trademark"]
+        version = request.POST["version"]
+        serial_number = request.POST["serial_number"]
+        failure = request.POST["failure"]
+        product_image1 = request.FILES.get("product_image1")
+        product_image2 = request.FILES.get("product_image2")
+        product_image3 = request.FILES.get("product_image3")
+        description = request.POST["description"]
+        Tickets.objects.create(ticket_number=ticket_number,customer_id=customer,product=product,trademark=trademark,version=version,serial_number=serial_number,failure=failure,product_image1=product_image1,product_image2=product_image2,product_image3=product_image3,description=description)
+        return redirect("all_tickets")
+    else:  
+        return render(request, "new_ticket.html")
+
+
+def create_warranty_product(request):
+    if request.method == "POST":
+        customer_id = request.POST["customer_id"]
+        customer = Customers.objects.get(id=customer_id)
+        ticket_id = request.POST["ticket_id"]
+        ticket = Tickets.objects.get(id=ticket_id)
+        description = request.POST["description"]
+        WarrantyProducts.objects.create(customer_id=customer,ticket_id=ticket,description=description)
+        return redirect("all_warranty_products")
+    else:  
+        return render(request, "new_warranty_product.html")
+
+
+def create_delivered_product(request):
+    if request.method == "POST":
+        ticket_id = request.POST["ticket_id"]
+        ticket = Tickets.objects.get(id=ticket_id)
+        DeliveredProducts.objects.create(ticket_id=ticket)
+        return redirect("all_delivered_products")
+    else:
+        return render(request, "new_delivered_product.html")
+    
+### TODOS LOS "READ" ###
+
+def show_all_customers(request):
+    try:
+        customers = Customers.objects.all()
+        context = {"customers":customers}
+        return render(request, "customers.html",context)
+    except:
+        return render(request, "customers.html")
+
+
+def show_all_tickets(request):
+    try:
+        tickets = Tickets.objects.all()
+        context = {"tickets":tickets}
+        return render(request, "tickets.html",context)
+    except:
+        return render(request, "tickets.html")
+
+
+def show_all_warranty_products(request):
+    try:
+        warranty_products = WarrantyProducts.objects.all()
+        context = {"warranty_products":warranty_products}
+        return render(request, "warranty_products.html",context)
+    except:
+        return render(request, "warranty_products.html")
+
+
+def show_all_delivered_products(request):
+    try:
+        delivered_products = DeliveredProducts.objects.all()
+        context = {"delivered_products":delivered_products}
+        return render(request, "delivered_products.html",context)
+    except:
+        return render(request, "delivered_products.html")
