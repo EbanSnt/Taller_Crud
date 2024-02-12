386c4b15f7d0c9a23e49075c43386eeab61843b7
EbanSnt <negociosebansnt@gmail.com>
2024-02-11 21:50:40 -0300

    debug
    debug=true

--- taller_crud/settings.py
+++ taller_crud/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-05kz6d1@0hky66ea-z=hst_n$q-r=)@*wb6trpltve&)w*8lvy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
